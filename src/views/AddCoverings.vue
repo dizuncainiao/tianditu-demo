38c5af96d5b23ac7fb009bd8b43a005bdef3e3fb
wangbo <781278657@qq.com>
2024-12-12 20:02:37 +0800

    feat: ✨️ Add function

--- src/views/AddCoverings.vue
+++ src/views/AddCoverings.vue
@@ -2,18 +2,26 @@
 import { onBeforeUnmount, onMounted } from 'vue'
 
 const map = ref()
+const lngLat = new T.LngLat(117.33001, 31.81325)
 //创建标注对象
-const marker = new T.Marker(new T.LngLat(117.33001, 31.81325))
+const marker = new T.Marker(lngLat)
+//创建信息窗口对象
+const infoWin = new T.InfoWindow()
+infoWin.setLngLat(new T.LngLat(117.33001, 31.81345))
+//设置信息窗口要显示的内容
+infoWin.setContent('数企（中国）总部')
 
 function initMap() {
   //初始化地图对象
   map.value = new T.Map('map-2')
   //设置显示地图的中心点和级别
-  map.value.centerAndZoom(new T.LngLat(117.33001, 31.81325), 18)
+  map.value.centerAndZoom(lngLat, 18)
   //允许鼠标双击放大地图
   map.value.enableScrollWheelZoom()
   //向地图上添加标注
   map.value.addOverLay(marker)
+  //向地图上添加信息窗口
+  map.value.addOverLay(infoWin)
 }
 
 onMounted(() => {
--- src/views/BasicMap.vue
+++ src/views/BasicMap.vue
@@ -7,14 +7,37 @@ function initMap() {
   //初始化地图对象
   map.value = new T.Map('map-1')
   //设置显示地图的中心点和级别
-  map.value.centerAndZoom(new T.LngLat(117.33001, 31.81325), 18)
+  map.value.centerAndZoom(new T.LngLat(116.40769, 39.89945), 12)
   // map.value.centerAndZoom(new T.LngLat(117.34282, 31.81764), 18)
   //允许鼠标双击放大地图
   map.value.enableScrollWheelZoom()
 }
 
-function destroyMap() {
-  map.value.destroy()
+// IP城市定位 fixme 官网例子的中接口都报错
+function getLocationOfIP() {
+  const lc = new T.LocalCity()
+  console.log(new T.LocalCityResult())
+  lc.location(function (e) {
+    alert(e.cityName)
+  })
+}
+
+// H5 定位
+function getLocationOfH5() {
+  const lo = new T.Geolocation()
+  console.log(lo, 'lo')
+  lo.getCurrentPosition(e => {
+    console.log(e, 'h5 定位')
+  })
+}
+
+function setMapCenter() {
+  map.value.centerAndZoom(new T.LngLat(117.33001, 31.81325), 12)
+}
+
+function handleClick() {
+  console.log('click')
+  setMapCenter()
 }
 
 onMounted(() => {
@@ -27,8 +50,11 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div class="basic-map-wrapper w-full h-full">
+  <div class="basic-map-wrapper w-full h-full relative">
     <div id="map-1" class="w-full h-full"></div>
+    <div class="toolbar absolute top-20px left-20px z-9999 flex gap-col-6px">
+      <button @click="handleClick">手动设置一个位置</button>
+    </div>
   </div>
 </template>
 
